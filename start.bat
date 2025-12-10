97533d7cac6bab2024d41a9143d6fcc6b962c520
Gaming God <hunter.huelsmann07@gmail.com>
2025-12-10 11:50:17 -0500

    Added start.bat and changed StoneTablet

---
+++ start.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Initialize player health
+set HP=10
+
+echo -
+echo Welcome to the Dungeon Crawl!
+echo You have %HP% health points.
+echo To play, use commands like 'dir' (look), 'type' (read), and 'cd' (move).
+echo Your first room is 'entrance'.
+echo Type: cd entrance
+echo -
+pause
